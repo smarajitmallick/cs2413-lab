6d21879847c10c4e404bd5f00aaebdfe03135fca
smarajitmallick <mallicksmarajit2006@gmail.com>
2026-02-19 21:14:06 +0000

    Edited the main file

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -15,8 +15,25 @@
 
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
 
- // TODO: implement
+    int i = m - 1;           // last real element in nums1
+    int j = n - 1;           // last element in nums2
+    int k = m + n - 1;       // last index of nums1
 
+    (void)nums1Size;
+    (void)nums2Size;
 
+    // Merge from the back
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--];
+        } else {
+            nums1[k--] = nums2[j--];
+        }
+    }
 
+    // If nums2 still has elements left, copy them
+    while (j >= 0) {
+        nums1[k--] = nums2[j--];
+    }
 }
+
