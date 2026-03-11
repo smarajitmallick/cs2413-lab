eb6af99b9932ca2134c16485e890a74eebef9393
smarajitmallick <mallicksmarajit2006@gmail.com>
2026-03-11 11:51:06 +0000

    Edited the main file

--- Assignment2/Question1/student.c
+++ Assignment2/Question1/student.c
@@ -27,7 +27,26 @@ struct TreeNode {
 struct TreeNode* bstSearch(struct TreeNode* root, int target) {
     // TODO: implement
     // Hint: Use the BST property to decide whether to go left or right.
-    (void)root;
-    (void)target;
+    // Traversing the tree untill we find the value or reach (NULL)
+
+    while(root != NULL)
+    {
+        //checking if the current node contains the target value 
+        if (root->val == target) 
+        {
+            return root; // returning pointer to this node
+        }
+        // If the target is smaller than current node value then searching in the left subtree
+        else if (target < root->val) 
+        {
+            root = root->left;
+        }
+        // If target is larger than current node value then searching in the right subtree
+        else 
+        {
+            root = root->right;
+        }
+    }
+
     return NULL;
 }
