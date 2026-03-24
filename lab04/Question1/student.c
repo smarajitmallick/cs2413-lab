681db0a03cdded6e92072c2d346cc2ff54c763e7
smarajitmallick <mallicksmarajit2006@gmail.com>
2026-03-24 01:56:21 +0000

    Edited the main file

--- lab04/Question1/student.c
+++ lab04/Question1/student.c
@@ -49,4 +49,15 @@ struct TreeNode {
 
 int maxDepth(struct TreeNode* root) {
   // TODO: implement
+
+  if(root == NULL)
+  {
+    return 0;
+  }
+
+  int left = maxDepth(root->left);
+  int right =  maxDepth(root->right);
+
+   return 1 + (left > right ? left : right);
+
 }
